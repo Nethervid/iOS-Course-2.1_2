0d0a3542c05c676cbba5044a337b55f502c101e4
Artem <artem2097@gmail.com>
2020-08-19 23:44:01 +0300

    Completed second exercise

--- Traffic Light/ViewController.swift
+++ Traffic Light/ViewController.swift
@@ -9,12 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var redLightLabel: UIView!
+    @IBOutlet weak var yellowLightLabel: UIView!
+    @IBOutlet weak var greenLightLabel: UIView!
+    @IBOutlet weak var stepperButton: UIButton!
+    
+    let alphaLightsOff: CGFloat = 0.3
+    let alphaLightsOn: CGFloat = 1
+    var section = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLightLabel.alpha = alphaLightsOff
+        yellowLightLabel.alpha = alphaLightsOff
+        greenLightLabel.alpha = alphaLightsOff
+        
+        redLightLabel.layer.cornerRadius = redLightLabel.frame.size.width/2
+        yellowLightLabel.layer.cornerRadius = yellowLightLabel.frame.size.width/2
+        greenLightLabel.layer.cornerRadius = greenLightLabel.frame.size.width/2
+        stepperButton.layer.cornerRadius = 15
     }
-
-
+    
+    @IBAction func stepperPressed() {
+        if let title = stepperButton.titleLabel?.text, title != "Next" {
+            stepperButton.setTitle("Next", for: .normal)
+        }
+        
+        switch section {
+        case 1:
+            redLightLabel.alpha = alphaLightsOn
+            greenLightLabel.alpha = alphaLightsOff
+            section = 2
+        case 2:
+            redLightLabel.alpha = alphaLightsOff
+            yellowLightLabel.alpha = alphaLightsOn
+            section = 3
+        case 3:
+            yellowLightLabel.alpha = alphaLightsOff
+            greenLightLabel.alpha = alphaLightsOn
+            section = 1
+        default:
+            break
+        }
+    }
+    
 }
 
